f6e60179307ce49a3166f258eaf1b5d11896b26e
ubaidillah <sayyidahmedubaidillah@saintek.unipdu.ac.id>
2025-11-25 12:20:20 +0700

    proyek phpmyadmin

---
+++ pemweb_ubed/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>data sekolah unipdu</h1>
+    <h2>smk unipdu</h2>
+    <a href="data_siswa.php">daftar siswa</a><br>
+    <a href="tambah_siswa.php">pendaftaran</a>
+
+</body>
+</html>
---
+++ pemweb_ubed/koneksi.php
@@ -0,0 +1,4 @@
+<?php
+$konek =mysqli_connect("localhost", "root", "root",  "root", "sekolahan");
+//server,user,password,nama database
+?>
